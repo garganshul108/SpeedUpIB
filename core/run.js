84900f21318ea8f3d62fbc65452b813b30ef0dce
garganshul108 <sci.agarg@gmail.com>
2019-06-22 14:03:12 +0530

    implemented Slider

--- core/run.js
+++ core/run.js
@@ -1,95 +1,51 @@
-function getInput() {
-    return new Promise((resolve, reject) => {
-        let input = parseFloat(prompt("Speed Factor: "));
-        // console.log(parseFloat(input));
-        // console.log(typeof(parseFloat(input)));
-        if (isNaN(input)) {
-            alert("Incorrect input for speed");
-            reject(new Error("Incorrect input for speed"));
-        } else if (input > 3.0) {
-            alert("Too high for Speed");
-            reject(new Error("Too high for Speed"));
-        } else resolve(parseFloat(input));
-    });
+let speedSlider;
+
+let speeding = false;
+
+function speedUp(event) {
+    if (speeding) {
+
+        let speedUpto = speedSlider.value;
+        // console.log(speedUpto);
+        document.querySelector('video').playbackRate = speedUpto;
+    }
+
 }
 
-function setSpeed() {
-    let t = document.getElementById('extension-slider');
-    document.querySelector('video').playbackRate = t.value;
-}
+function waitingForAngular() {
 
-function setInputSlider() {
-    alert('setinput');
-    let t = document.getElementById('extension-slider');
-    t.value = this.value;
-}
 
-setTimeout(() => {
     console.log("Anshul's Extension Works");
     const target = document.querySelector('video').parentElement;
 
     let sliderform = document.createElement('div');
 
+    sliderform.innerHTML =
+        `
+            <input type="range" min="0.25" max="3.0" value="1.0" step="0.25" id="speedSlider" class="slider"/>    
+        `;
+
     let sliderformclass = document.createAttribute('class');
     sliderformclass.value = 'slidercontainer';
     sliderform.setAttributeNode(sliderformclass);
 
-    let slider = document.createElement('input');
-
-    let slideroninput = document.createAttribute('mousedown');
-    slideroninput.value = 'setInputSlider()';
-    slider.setAttributeNode(slideroninput);
-
-    let slidertype = document.createAttribute('type');
-    slidertype.value = 'range';
-    slider.setAttributeNode(slidertype);
-
-    let slidermin = document.createAttribute('min');
-    slidermin.value = '0.5';
-    slider.setAttributeNode(slidermin);
-
-    let slidermax = document.createAttribute('max');
-    slidermax.value = '3.0';
-    slider.setAttributeNode(slidermax);
-
-    let sliderdefault = document.createAttribute('value');
-    sliderdefault.value = '1.0';
-    slider.setAttributeNode(sliderdefault);
-
-    let sliderid = document.createAttribute('id');
-    sliderid.value = 'extension-slider';
-    slider.setAttributeNode(sliderid);
-
-    let sliderclass = document.createAttribute('class');
-    sliderclass.value = 'slider';
-    slider.setAttributeNode(sliderclass);
-
-
-    sliderform.appendChild(slider);
-
-
-
-    let speedButton = document.createElement('button');
-
-    let buttonid = document.createAttribute('id');
-    buttonid.value = 'click-btn';
-    speedButton.setAttributeNode(buttonid);
-
-
-    let buttonclass = document.createAttribute('class');
-    buttonclass.value = 'click-btn-class';
-    speedButton.setAttributeNode(buttonclass);
-
-    let buttononclick = document.createAttribute('onclick');
-    buttonclass.value = 'setSpeed()';
-    speedButton.setAttributeNode(buttononclick);
-
-
-    sliderform.appendChild(speedButton);
-
     target.appendChild(sliderform);
 
+    speedSlider = document.getElementById('speedSlider');
+    speedSlider.addEventListener('mousedown', function(event) {
+        speeding = true;
+        speedUp(event);
+    });
+    speedSlider.addEventListener('mousemove', function(event) {
+        speedUp(event);
+    });
+    speedSlider.addEventListener('mouseup', function() {
+        setTimeout(() => {
+            speeding = false;
+        }, 100);
+    });
 
 
+}
 
-}, 2000);
+setTimeout(waitingForAngular, 2000);
