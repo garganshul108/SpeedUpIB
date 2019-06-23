fd48a7e2e4bc265f4f5956758f4fc543daf2964b
garganshul108 <sci.agarg@gmail.com>
2019-06-23 11:03:17 +0530

    solved multiple video problem

--- core/run.js
+++ core/run.js
@@ -1,5 +1,7 @@
 let speedSlider;
 
+let videoTabs;
+
 let speeding = false;
 
 function speedUp(event) {
@@ -7,7 +9,9 @@ function speedUp(event) {
 
         let speedUpto = speedSlider.value;
         // console.log(speedUpto);
-        document.querySelector('video').playbackRate = speedUpto;
+        for (video of videoTabs) {
+            video.playbackRate = speedUpto;
+        }
     }
 
 }
@@ -17,19 +21,23 @@ function waitingForAngular() {
 
     console.log("Anshul's Extension Works");
     const target = document.querySelector('video').parentElement;
-    document.querySelector('video').setAttribute('controlslist', 'download');
-    let sliderform = document.createElement('div');
+    videoTabs = document.getElementsByTagName('video');
+    // added download option for each video on page
+    for (video of videoTabs) {
+        video.setAttribute('controlslist', 'download');
+    }
 
+
+    let sliderform = document.createElement('div');
     sliderform.innerHTML =
         `
             <input type="range" min="0.25" max="3.0" value="1.0" step="0.25" id="speedSlider" class="slider"/>    
         `;
-
     let sliderformclass = document.createAttribute('class');
     sliderformclass.value = 'slidercontainer';
     sliderform.setAttributeNode(sliderformclass);
 
-    target.appendChild(sliderform);
+    target.insertBefore(sliderform, target.firstChild);
 
     speedSlider = document.getElementById('speedSlider');
     speedSlider.addEventListener('mousedown', function(event) {
