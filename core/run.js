b375448945799ef8470d36acbe5ecbc5edfe877e
garganshul108 <sci.agarg@gmail.com>
2019-06-23 11:22:08 +0530

    tag of speed added

--- core/run.js
+++ core/run.js
@@ -1,17 +1,20 @@
 let speedSlider;
-
 let videoTabs;
-
+let speedDisplay;
 let speeding = false;
+
 
 function speedUp(event) {
     if (speeding) {
 
         let speedUpto = speedSlider.value;
-        // console.log(speedUpto);
+        console.log(speedUpto);
+
         for (video of videoTabs) {
             video.playbackRate = speedUpto;
         }
+
+        speedDisplay.innerHTML = speedUpto;
     }
 
 }
@@ -22,7 +25,9 @@ function waitingForAngular() {
     console.log("Anshul's Extension Works");
     const target = document.querySelector('video').parentElement;
     videoTabs = document.getElementsByTagName('video');
+
     // added download option for each video on page
+
     for (video of videoTabs) {
         video.setAttribute('controlslist', 'download');
     }
@@ -30,7 +35,8 @@ function waitingForAngular() {
 
     let sliderform = document.createElement('div');
     sliderform.innerHTML =
-        `
+        ` 
+            <h5>Speed Factor :x<span id="speedDisplay">1</span></h5>
             <input type="range" min="0.25" max="3.0" value="1.0" step="0.25" id="speedSlider" class="slider"/>    
         `;
     let sliderformclass = document.createAttribute('class');
@@ -38,19 +44,23 @@ function waitingForAngular() {
     sliderform.setAttributeNode(sliderformclass);
 
     target.insertBefore(sliderform, target.firstChild);
-
+    speedDisplay = document.getElementById('speedDisplay');
     speedSlider = document.getElementById('speedSlider');
+
     speedSlider.addEventListener('mousedown', function(event) {
         speeding = true;
         speedUp(event);
     });
+
     speedSlider.addEventListener('mousemove', function(event) {
         speedUp(event);
     });
+
     speedSlider.addEventListener('mouseup', function() {
         setTimeout(() => {
             speeding = false;
         }, 100);
+        // speedDisplay.innerHTML = speedUpto;
     });
 
 
